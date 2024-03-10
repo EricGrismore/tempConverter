07fbe157fba2d60bf9149142e08e5454c0b98bae
grizz <grizz@DESKTOP-492A8HG>
2024-03-10 19:18:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace tempConverter
+{
+    class temps
+    {
+        public static void Main(string[] args)
+        {
+            string numberType;
+            int intNumToConvert;
+            double dblNumToConvert;
+            char type = menu();
+            while (type != 'Q')
+            {
+                try
+                {
+                    Console.WriteLine("Do you want to enter a whole number or real number? (type 'whole' or 'real') ");
+                    numberType = Console.ReadLine();
+                    while (numberType != "whole" && numberType != "real")
+                    {
+                        Console.WriteLine("Please try again, type either 'whole' or 'real'");
+                        numberType = Console.ReadLine();
+                    }
+                    Console.WriteLine("Enter the temp you want converted");
+                    if (numberType == "whole")
+                    {
+                        intNumToConvert = int.Parse(Console.ReadLine());
+                        convertTemp(type, intNumToConvert);
+                    }
+                    else
+                    {
+                        dblNumToConvert = double.Parse(Console.ReadLine());
+                        convertTemp(type, dblNumToConvert);
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("You have entered bad data");
+                    Console.WriteLine(e.Message);
+                }
+                catch
+                {
+                    Console.WriteLine("An error occurred, please try again.");
+                }
+                type = menu();
+
+            }
+
+        }
+        private static void convertTemp(char type, int intNumToConvert)
+        {
+            int Converted;
+
+            if (type == 'F')
+            {
+                Converted = (intNumToConvert * 9 / 5) + 32;
+            }
+            else
+            {
+                Converted = (intNumToConvert - 32) * 5 / 9;
+            }
+            Console.Write(Converted);
+        }
+        private static void convertTemp(char type, double dblNumToConvert)
+        {
+            double Converted;
+
+            if (type == 'F')
+            {
+                Converted = (dblNumToConvert * 9 / 5) + 32;
+            }
+            else
+            {
+                Converted = (dblNumToConvert - 32) * 5 / 9;
+            }
+            Console.Write(Converted);
+        }
+        private static char menu()
+        {
+            Console.Write("What temperature are you converting to: ");
+            Console.WriteLine("C=celsius, F=fahrenheit, Q=quit");
+            int t = Console.Read();
+            Console.ReadLine();
+            char type = Convert.ToChar(t);
+            type = char.ToUpper(type);
+            while (type != 'C' && type != 'F' && type != 'Q')
+            {
+                Console.WriteLine("Try again, select C=celsius, F=fahrenheit, Q=quit");
+                t = Console.Read();
+                Console.ReadLine();
+                type = Convert.ToChar(t);
+                type = char.ToUpper(type);
+            }
+            return type;
+        }
+
+    }
+}
